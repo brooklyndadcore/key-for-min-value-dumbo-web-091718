5e2dc3e4267098891eead6de6776b1cd0f0ca4a5
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-20 20:23:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.each do |k , v|
+    name_hash.each do |k1, v1|
+      if v < v1 && v != v1
+        return k
+      end
+    end
+  end
 end
